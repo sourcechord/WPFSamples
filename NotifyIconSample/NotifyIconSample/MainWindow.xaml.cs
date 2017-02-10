723f1f7d82ad6e8dd7e6f6373467d979c3e693dd
minami <sourcechord@gmail.com>
2017-02-11 04:44:21 +0900

    Add sample project 'NotifyIconSample'

---
+++ NotifyIconSample/NotifyIconSample/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace NotifyIconSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NotifyIconEx _notify;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var iconPath = new Uri("pack://application:,,,/NotifyIconSample;component/Icon1.ico", UriKind.Absolute);
+            var menu = (ContextMenu)this.FindResource("sampleWinMenu");
+            //var menu = (ContextMenu)this.FindResource("sampleAppMenu");
+            this._notify = new NotifyIconEx(iconPath, "Notify Title", menu);
+
+            this._notify.DoubleClick += (_, __) => { this.ShowWindow(); };
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // クローズ処理をキャンセルして、タスクバーの表示も消す
+            e.Cancel = true;
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // ウィンドウを閉じる際に、タスクトレイのアイコンを削除する。
+            this._notify.Dispose();
+        }
+
+        private void ShowWindow()
+        {
+            // ウィンドウ表示&最前面に持ってくる
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+                this.WindowState = System.Windows.WindowState.Normal;
+
+            this.Show();
+            this.Activate();
+            this.ShowInTaskbar = true;
+        }
+
+        private void btnSetTitle_Click(object sender, RoutedEventArgs e)
+        {
+            this._notify.Text = this.txtTitle.Text;
+        }
+
+        private void btnShowBalloon_Click(object sender, RoutedEventArgs e)
+        {
+            var icon = (ToolTipIconEx)this.cmbIcon.SelectedValue;
+            var title = this.txtBalloonTitle.Text;
+            var text = this.txtBalloonText.Text;
+
+            this._notify.ShowBalloonTip(100, title, text, icon);
+        }
+
+
+        #region タスクトレイのContextMenu用のイベント定義
+        private void MenuItem_Show_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShowWindow();
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void MenuItem_Show_Balloon_Click(object sender, RoutedEventArgs e)
+        {
+            this._notify.ShowBalloonTip(1000, "tipTitle", "tipText", ToolTipIconEx.Info);
+        }
+        #endregion
+
+    }
+}
---
+++ NotifyIconSample/NotifyIconSample/NotifyIconEx.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyIconSample
+{
+    public enum ToolTipIconEx
+    {
+        None = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+    public class NotifyIconEx : IDisposable
+    {
+        private System.Windows.Forms.NotifyIcon _notify;
+
+        public string Text
+        {
+            get { return this._notify.Text; }
+            set { this._notify.Text = value; }
+        }
+
+        public bool Visible
+        {
+            get { return this._notify.Visible; }
+            set { this._notify.Visible = value; }
+        }
+
+        public Uri IconPath
+        {
+            set
+            {
+                if (value == null) { return; }
+                var iconStream = System.Windows.Application.GetResourceStream(value).Stream;
+                this._notify.Icon = new System.Drawing.Icon(iconStream);
+            }
+        }
+
+        public System.Windows.Controls.ContextMenu ContextMenu { get; set; }
+
+        public ToolTipIconEx BalloonTipIcon
+        {
+            get { return (ToolTipIconEx)this._notify.BalloonTipIcon; }
+            set { this._notify.BalloonTipIcon = (System.Windows.Forms.ToolTipIcon)value; }
+        }
+
+        public string BalloonTipTitle
+        {
+            get { return this._notify.BalloonTipTitle; }
+            set { this._notify.BalloonTipTitle = value; }
+        }
+
+        public string BalloonTipText
+        {
+            get { return this._notify.BalloonTipText; }
+            set { this._notify.BalloonTipText = value; }
+        }
+
+        public void ShowBalloonTip(int timeout)
+        {
+            this._notify.ShowBalloonTip(timeout);
+        }
+
+        public void ShowBalloonTip(int timeout, string tipTitle, string tipText, ToolTipIconEx tipIcon)
+        {
+            var icon = (System.Windows.Forms.ToolTipIcon)tipIcon;
+            this._notify.ShowBalloonTip(timeout, tipTitle, tipText, icon);
+        }
+
+        public NotifyIconEx()
+            : this(null) { }
+
+        public NotifyIconEx(Uri iconPath)
+            : this(iconPath, null) { }
+
+        public NotifyIconEx(Uri iconPath, string text)
+            : this(iconPath, text, null) {}
+
+        public NotifyIconEx(Uri iconPath, string text, System.Windows.Controls.ContextMenu menu)
+        {
+            // 各種プロパティを初期化
+            this._notify = new System.Windows.Forms.NotifyIcon();
+            this.IconPath = iconPath;
+            this.Text = text;
+            this.ContextMenu = menu;
+
+            // マウス右ボタンUpのタイミングで、ContextMenuの表示を行う
+            // ダミーの透明ウィンドウを表示し、このウィンドウのアクティブ状態を用いてContextMenuの表示/非表示を切り替える
+            this._notify.MouseUp += (s, e) =>
+            {
+                if (e.Button != System.Windows.Forms.MouseButtons.Right) { return; }
+
+                var win = new System.Windows.Window()
+                {
+                    WindowStyle = System.Windows.WindowStyle.None,
+                    ShowInTaskbar = false,
+                    AllowsTransparency = true,
+                    Background = System.Windows.Media.Brushes.Transparent,
+                    Content = new System.Windows.Controls.Grid(),
+                    ContextMenu = this.ContextMenu
+                };
+
+                var isClosed = false;
+                win.Activated += (_, __) =>
+                {
+                    if (win.ContextMenu != null)
+                    {
+                        win.ContextMenu.IsOpen = true;
+                    }
+                };
+                win.Closing += (_, __) =>
+                {
+                    isClosed = true;
+                };
+
+                win.Deactivated += (_, __) =>
+                {
+                    if (win.ContextMenu != null)
+                    {
+                        win.ContextMenu.IsOpen = false;
+                    }
+                    if (!isClosed)
+                    {
+                        win.Close();
+                    }
+                };
+                
+                // ダミーウィンドウ表示&アクティブ化をする。
+                // ⇒これがActivatedイベントで、ContextMenuが表示される
+                win.Show();
+                win.Activate();
+            };
+
+            this._notify.Visible = true;
+        }
+
+        #region NotifyIconクラスの各種イベントをラップする
+        public event EventHandler BalloonTipClicked
+        {
+            add { this._notify.BalloonTipClicked += value; }
+            remove { this._notify.BalloonTipClicked -= value; }
+        }
+
+        public event EventHandler BalloonTipClosed
+        {
+            add { this._notify.BalloonTipClosed += value; }
+            remove { this._notify.BalloonTipClosed -= value; }
+        }
+
+        public event EventHandler BalloonTipShown
+        {
+            add { this._notify.BalloonTipShown += value; }
+            remove { this._notify.BalloonTipShown -= value; }
+        }
+
+        public event EventHandler Click
+        {
+            add { this._notify.Click += value; }
+            remove { this._notify.Click -= value; }
+        }
+
+        public event EventHandler Disposed
+        {
+            add { this._notify.Disposed += value; }
+            remove { this._notify.Disposed -= value; }
+        }
+
+        public event EventHandler DoubleClick
+        {
+            add { this._notify.DoubleClick += value; }
+            remove { this._notify.DoubleClick -= value; }
+        }
+        #endregion
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this._notify.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+    }
+}
